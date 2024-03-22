954b6a74f55ded34154be14c33b5e02764d1847d
Abdullah Gillani <72945262+agblaze@users.noreply.github.com>
2024-03-22 09:40:16 +0000

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abdullah Ejaz",
+  title: "Photographer & Content Creator",
+  email: "abdullahgillani042gmail.com",
+  gitHub: "agblaze",
+  instagram: "ag_graaphy",
+  linkedIn: "abdullah_gillani",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
